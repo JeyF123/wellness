98b79a0cb223be8e115169a013b1345df64b9387
JeyF123 <jeyf123@gmail.com>
2021-01-27 21:01:19 +0100

    Trigger rebuild

---
+++ github.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+git init
+sleep 2
+git commit -m "new commit"
+sleep 2
+git add .
+sleep 2
+git branch -M main
+sleep 2
+git remote add origin https://github.com/JeyF123/wellness.git
+sleep 2
+git push -u origin main
+sleep 2
+git commit --allow-empty -m "Trigger rebuild"
+sleep 10
+git push
